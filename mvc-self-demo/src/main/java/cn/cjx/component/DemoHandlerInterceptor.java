5a26bb613f1b4350fe4cbd7fdf28bc07729b593c
maple <343715674@qq.com>
2020-05-08 18:08:45 +0800

    【spring mvc】

---
+++ mvc-self-demo/src/main/java/cn/cjx/component/DemoHandlerInterceptor.java
@@ -0,0 +1,34 @@
+package cn.cjx.component;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @功能描述:
+ * @使用对象:xx系统
+ * @创建日期: 2020/5/8 18:05
+ * @创建人:陈俊旋
+ */
+public class DemoHandlerInterceptor implements CjxHandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String username = request.getParameter("username");
+        if (username==null){
+            username = (String) request.getAttribute("username");
+        }
+//        if (username==null && )
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
